83b1f820f1c8b35c5a32095d5c34179d10774745
TylerL <tluckewicz@gmail.com>
2021-08-13 10:01:23 -0400

    get correct player data when going to a team page

--- src/App.js
+++ src/App.js
@@ -35,7 +35,6 @@ function App() {
           <PlayersPage />
         </Route>
       </Switch>
-
     </div>
   );
 }
--- src/components/PlayersPage.js
+++ src/components/PlayersPage.js
@@ -1,9 +1,25 @@
-
+import { useState, useEffect } from 'react';
 
 function PlayersPage() {
+    // Find the team id from the URL
     const pathName = window.location.pathname;
     const teamId = pathName.split('/')[2];
     console.log(teamId);
+
+
+    const baseURL = 'http://127.0.0.1:9393';
+
+    const [players, setPlayers] = useState([]);
+  
+    useEffect(() => {
+      fetch(`${baseURL}/players/${teamId}`)
+        .then(res => res.json())
+        .then(resPlayers => {
+          setPlayers(resPlayers);
+        })
+    }, []);
+
+    console.log(players);
 
     return (
         <div>
